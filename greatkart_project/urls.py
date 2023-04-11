10975e80ec58374cefba15d4534aab32b99ff410
rivka_yuliia <juliacreadora@gmail.com>
2023-04-11 14:53:03 +0300

    media upload

--- greatkart_project/urls.py
+++ greatkart_project/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import HomeView
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
