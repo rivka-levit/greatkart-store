8d24ea0d546a2924517b80e28ae2c315e358f4c2
rivka_yuliia <juliacreadora@gmail.com>
2023-04-13 05:11:16 +0300

    change main to all_product name

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from .views import StoreView
 app_name = 'store'
 
 urlpatterns = [
-    path('', StoreView.as_view(), name='main'),
+    path('', StoreView.as_view(), name='all_products'),
     path('<slug:category_slug>/', StoreView.as_view(), name='by_category')
 ]
