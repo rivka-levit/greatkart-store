f85a248ccaaad91530354808e0bf81f16b69ce93
rivka_yuliia <juliacreadora@gmail.com>
2023-04-13 14:09:32 +0300

    category context_processors

--- store/views.py
+++ store/views.py
@@ -19,6 +19,5 @@ class StoreView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['categories'] = Category.objects.all()
         context['products_count'] = self.get_queryset().count()
         return context
