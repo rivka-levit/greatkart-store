24ea0527c96da471651b6fc607133fa40b1d0a55
rivka_yuliia <juliacreadora@gmail.com>
2023-04-14 15:40:32 +0300

    implement Cart and CartItem models

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,28 @@
 from django.db import models
+from store.models import Product
 
 
 class Cart(models.Model):
-    pass
+    name = models.CharField(max_length=50, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'cart'
+        verbose_name_plural = 'carts'
+
+    def __str__(self):
+        return self.name
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'cart item'
+        verbose_name_plural = 'cart items'
+
+    def __str__(self):
+        return self.product.product_name
