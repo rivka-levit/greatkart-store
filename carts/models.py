33e8809cf1331832ccbca1daf3a8425ac00b20f5
rivka_yuliia <juliacreadora@gmail.com>
2023-04-15 23:56:38 +0300

    related_name to cart_items

--- carts/models.py
+++ carts/models.py
@@ -16,7 +16,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True, related_name='cart_items')
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
