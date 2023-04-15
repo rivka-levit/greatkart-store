e471080e8c64f9e1827cf93712ef600014899085
rivka_yuliia <juliacreadora@gmail.com>
2023-04-16 00:25:23 +0300

    reformat code

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import redirect
-from django.views.generic import DetailView, TemplateView, ListView
+from django.views.generic import ListView
 from .models import Cart, CartItem
 from store.models import Product
-from django.http import HttpResponseRedirect
 
 
 class CartView(ListView):
@@ -13,10 +12,7 @@ class CartView(ListView):
     def get_queryset(self):
         user = self.request.user
         if user.is_authenticated is False:
-            try:
-                cart = Cart.objects.get(cart_id=_cart_id(self.request))
-            except Cart.DoesNotExist:
-                cart = Cart.objects.create(cart_id=_cart_id(self.request))
+            cart = _get_cart(self.request)
             cart_items = super(CartView, self).get_queryset().filter(cart=cart)
         return cart_items
 
@@ -28,13 +24,18 @@ def _cart_id(request):
     return cart
 
 
+def _get_cart(request):
+    cart_id = _cart_id(request)
+    try:
+        cart = Cart.objects.get(cart_id=cart_id)
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=cart_id)
+    return cart
+
+
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(cart_id=_cart_id(request))
-    cart.save()
+    cart = _get_cart(request)
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1
