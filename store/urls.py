8ea00491cca8c82aba639aef7aa371eb193f8b95
rivka_yuliia <juliacreadora@gmail.com>
2023-04-18 14:38:14 +0300

    change category url

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ app_name = 'store'
 urlpatterns = [
     path('', StoreView.as_view(), name='all_products'),
     path('search/', SearchResultsView.as_view(), name='search'),
-    path('<slug:category_slug>/', StoreView.as_view(), name='by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/',
+    path('category/<slug:category_slug>/', StoreView.as_view(), name='by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/',
          ProductDetailView.as_view(), name='product_detail'),
 ]
