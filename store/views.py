e1ac5540a61dc43c74ee2ec13bb4b7a93d9c8636
rivka_yuliia <juliacreadora@gmail.com>
2023-04-19 00:09:42 +0300

    search queryset filtering

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, View
 from .models import Product
 from category.models import Category
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 
 
 class StoreView(ListView):
@@ -39,14 +40,16 @@ class SearchResultsView(ListView):
     template_name = 'store/search-result.html'
     context_object_name = 'products'
     paginate_by = 6
-    ordering = ['id']
+    ordering = ['-created_date']
 
     def get_queryset(self):
         products = super(SearchResultsView, self).get_queryset().filter(is_available=True)
         query = self.request.GET.get("q")
         if not query:
             return None
-        return products.filter(product_name__contains=query)
+        return products.filter(
+            Q(product_name__icontains=query) | Q(description__icontains=query)
+        )
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
