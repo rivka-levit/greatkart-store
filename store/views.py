5cc628ad7971541d247c6fbe4d1d667b5805a21c
rivka_yuliia <juliacreadora@gmail.com>
2023-04-18 23:37:32 +0300

    incluedes for nav-left and products-count

--- store/views.py
+++ store/views.py
@@ -47,3 +47,8 @@ class SearchResultsView(ListView):
         if not query:
             return None
         return products.filter(product_name__contains=query)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products_count'] = self.get_queryset().count()
+        return context
