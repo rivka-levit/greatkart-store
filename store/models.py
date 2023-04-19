69c4907f425e81c9351913cfcc5e94cc0453e5ca
rivka_yuliia <juliacreadora@gmail.com>
2023-04-19 15:24:54 +0300

    create variation model

--- store/models.py
+++ store/models.py
@@ -20,3 +20,24 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size')
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variations')
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'product variation'
+        verbose_name_plural = 'product variations'
+
+    def __str__(self):
+        return f'{self.product.product_name} variation'
