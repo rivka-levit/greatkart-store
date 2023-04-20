b4f3c054ce6406c70d14f5a2bcacc17fff135caa
rivka_yuliia <juliacreadora@gmail.com>
2023-04-20 23:38:30 +0300

    create login.html, view, url

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class RegisterView(TemplateView):
     template_name = 'accounts/register.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'accounts/login.html'
