0993e7d838c04c4f9ebadf4acf3ced33eadac1b2
rivka_yuliia <juliacreadora@gmail.com>
2023-04-21 00:28:12 +0300

    create registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from .forms import RegistrationForm
 from django.shortcuts import reverse
 
 
-class RegisterView(TemplateView):
+class RegisterView(FormView):
     template_name = 'accounts/register.html'
+    form_class = RegistrationForm
+    success_url = 'home'
 
 
 class LoginView(TemplateView):
