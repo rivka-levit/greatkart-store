de67a4877f1c0870f50d2c7457e89d95e6e8c33f
rivka_yuliia <juliacreadora@gmail.com>
2023-04-21 03:27:24 +0300

    post for RegisterView

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,35 @@
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 from .forms import RegistrationForm
-from django.shortcuts import reverse
+from .models import Account
+from django.shortcuts import reverse, redirect
 
 
-class RegisterView(FormView):
+class RegisterView(CreateView):
+    model = Account
     template_name = 'accounts/register.html'
     form_class = RegistrationForm
-    success_url = 'home'
+
+    def post(self, request, *args, **kwargs):
+        super(RegisterView, self).post(request)
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+        return redirect('accounts:login')
 
 
 class LoginView(TemplateView):
