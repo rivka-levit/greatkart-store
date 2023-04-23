79490d65fdbe4567c861920538abb3109dbb9791
rivka_yuliia <juliacreadora@gmail.com>
2023-04-24 00:26:52 +0300

    accounts to dashboard path

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout, name='logout'),
-    path('activate/<uidb64>/<token>', activate, name='activate'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('activate/<uidb64>/<token>', activate, name='activate'),
 ]
