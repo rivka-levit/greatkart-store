52290cb659ce2db5942b7d963396b4ba2d41dd99
rivka-yuliia <juliacreadora@gmail.com>
2023-04-30 14:41:58 +0300

    create Order model

--- orders/models.py
+++ orders/models.py
@@ -14,3 +14,34 @@ class Payment(models.Model):
         return self.payment_id
 
 
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled')
+    )
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True,
+                             related_name='orders')
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True)
+    order_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    total = models.DecimalField(decimal_places=2)
+    tax = models.DecimalField(decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='New')
+    ip = models.CharField(max_length=20, blank=True)
+    order_note = models.CharField(max_length=100, blank=True)
+    is_ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} -- Order number: {self.order_number}'
