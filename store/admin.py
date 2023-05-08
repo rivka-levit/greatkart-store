e69521db3a13b80dace8ecebf3959f6946de6134
rivka-yuliia <juliacreadora@gmail.com>
2023-05-08 14:12:36 +0300

    review model and admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, Variation, ReviewRating
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -14,5 +14,11 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable = ('is_active',)
 
 
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'subject', 'rating', 'status', 'updated_at')
+    readonly_fields = ('ip', 'created_at', 'updated_at')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from accounts.models import Account
 
 
 class Product(models.Model):
@@ -58,3 +59,22 @@ class Variation(models.Model):
     def __str__(self):
         return f'{self.variation_category.capitalize()} - ' \
                f'{self.variation_value.capitalize()}'
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'review'
+        verbose_name_plural = 'reviews'
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}: {self.subject}'
