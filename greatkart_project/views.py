072607a088cd95dbf5c64d319d2671ffcab7c591
rivka-yuliia <juliacreadora@gmail.com>
2023-05-18 20:58:55 +0300

    ordering HomeView

--- greatkart_project/views.py
+++ greatkart_project/views.py
@@ -6,9 +6,8 @@ class HomeView(ListView):
     model = Product
     template_name = 'home.html'
     context_object_name = 'products'
+    ordering = ['-created_date']
 
     def get_queryset(self):
         queryset = super(HomeView, self).get_queryset()
         return queryset.filter(is_available=True)
-
-    # def paginate_queryset(self):
